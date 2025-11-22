5595010ba3351d94c38673880b70ded44f788a72
sriram t <sriramagaram07@gmail.com>
2025-11-22 13:56:09 +0530

    Update Pdfviewer component to use @react-pdf-viewer for improved PDF rendering and search functionality; update dependencies in package.json

--- src/features/clientviewer/components/Pdfviewer.tsx
+++ src/features/clientviewer/components/Pdfviewer.tsx
@@ -1,29 +1,29 @@
-import { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import { searchPlugin } from "@react-pdf-viewer/search";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
-
-  pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url,
-  ).toString();
-
-
-const Pdfviewer = () => {
-
-  const [pageNumber , setPageNumber] = useState()
-
-
-  function onDocumentLoadSuccess(data:any) {
-    console.log('document is loaded :' , data)
-  }
+export default function PdfViewer() {
+  const defaultLayout = defaultLayoutPlugin();
+  const searchPluginInstance = searchPlugin({
+    enableShortcuts:true,
+    keyword: [{
+      keyword: 'Angular',
+      matchCase: true,
+      wholeWords: true,
+    }]
+    
+  });
 
   return (
-    <div>
-      <Document file={`/angular_mastery_roadmap.pdf`} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
+    <div style={{ height: "100vh" }}>
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+        <Viewer
+          fileUrl="/angular_mastery_roadmap.pdf"
+          plugins={[defaultLayout, searchPluginInstance]}
+        />
+      </Worker>
     </div>
-  )
+  );
 }
-
-export default Pdfviewer
